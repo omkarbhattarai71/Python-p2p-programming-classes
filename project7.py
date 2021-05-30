0e9629e0fad60254c3fd374e6f462450ab1b2a70
omkarbhattarai71 <83543183+omkarbhattarai71@users.noreply.github.com>
2021-05-31 02:22:36 +0545

    Inverted Right Angled Triangle.py

---
+++ project7.py
@@ -0,0 +1,14 @@
+'''Inverted Right Angled Triangle 
+* * * * * * * * 
+* * * * * * * 
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*                    '''
+
+for i in range(9,1,-1):
+    for j in range(1,i):
+        print("*",end=' ')
+    print()
