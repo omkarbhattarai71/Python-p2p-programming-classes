dd0a5a78da88568dfbc66acfcfb02082968523b2
omkarbhattarai71 <83543183+omkarbhattarai71@users.noreply.github.com>
2021-05-31 01:41:57 +0545

    project1.py

---
+++ project1.py
@@ -0,0 +1,9 @@
+# Take two numbers as input and perform all the arithmetic operations in it
+a=int(input("Enter first number::  "))
+b=int(input("Enter second number:: "))
+print("sum is: {} ".format(a+b))
+print("difference is: {} ".format(a-b))
+print("Multiplication is: {} ".format(a*b))
+print("Float Division is: {}".format(a/b))
+print("Integer Division is: {}".format(a//b))
+print("Remainder is: {} ".format(a%b))
