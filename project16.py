3c09d7f75c0d0841a09ec251fedc3db2542bf099
omkarbhattarai71 <83543183+omkarbhattarai71@users.noreply.github.com>
2021-06-06 23:04:50 +0545

    Create project16.py
    to reverses a string

---
+++ project16.py
@@ -0,0 +1,18 @@
+# Write a Python function to reverses a string
+
+
+def reverse_string(strn):
+    s=''
+    for i in strn:
+        s=i+s
+    print("Reversed string is:",s)
+        
+
+
+reverse_string("omkarbhattarai")
+
+
+
+
+Output:
+  Reversed string is: iarattahbrakmo
