7003a126a9a3d2122eb73c5066d0fd4496f0076f
omkarbhattarai71 <83543183+omkarbhattarai71@users.noreply.github.com>
2021-06-06 23:23:58 +0545

    size of a Set in Python

---
+++ project17.py
@@ -0,0 +1,16 @@
+# . Find the size of a Set in Python
+
+sets={'omkar','p2p','python','cse',1,2,3}
+s=0
+for i in sets:
+    s+=1
+    
+print("Size is:::",s)
+
+
+#print("Size is::",len(sets))
+
+
+Output::
+    Size is::: 7
+    
